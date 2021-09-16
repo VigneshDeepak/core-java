c322e25ad86f7a84a7af9f5ff7d390efe16d712e
VigneshDeepak <35281901+VigneshDeepak@users.noreply.github.com>
2021-09-16 12:54:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 int a=10,b=20,c=11,d=5;
+		 int x=a+b;
+		 int y=c-d;
+		 int z=x*y;
+		 int w=z/y;
+		 System.out.println(x+" "+y+" "+z+" "+w);// cotations used to concatenate the operations
+
+	}
+
+}
